a02e435a44c4f39ea3c1320cf38ebf6d43ec0b02
Thiago Santos Bonfim <thiago1343@gmail.com>
2021-02-20 17:23:01 +0000

    Migrando as pastas e arquivos para o git

---
+++ index.php
@@ -0,0 +1,19 @@
+<html lang="pt-br">
+<head>
+   <meta charset="utf-8"/>
+   <title> Projeto Login</title>    
+   <link rel="stylesheet" href="CSS/estilo.css">
+</head>
+<body>
+<div id="corpo-form">
+   <h1> ENTRAR </h1>
+   <form method="POST" action="processa.php">
+      <input type="email" placeholder="usuário">
+      <input type="password" placeholder="senha">
+      <input type="submit" value="acessar">
+      <a href=""> Ainda não é inscrito? <strong>Cadastre-se!</strong> </a>
+   </form>
+</div>
+
+</body>
+</html>
