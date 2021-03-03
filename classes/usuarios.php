4f0e1b8db1b872ef727a5490176735b646220ceb
Thiago1343 <thiago1343@gmail.com>
2021-03-03 16:30:58 +0000

    verifica se o usuário está cadastrado e em seguida faz o login

--- classes/usuarios.php
+++ classes/usuarios.php
@@ -49,13 +49,25 @@ Class Usuario
    }
    public function logar($email, $senha)
    {
-
-
-
-
+   global $pdo;
+   //verificar se o email e senha estão cad no sistema. caso estiver, então 
+   $sql = $pdo->prepare("SELECT id_usuario FROM usuarios WHERE email =:e AND senha = :s");
+   $sql->bindValue(":e",$email);
+   $sql->bindValue(":s",$senha);
+   $sql->execute();
+   if($sql->rowCount() > 0)
+   {
+        //entrar no sistema (sessão)
+        $dado =  &sql->fetch();
+        session_start();
+        $_SESSION['id_usuario'] = $dado['id_usuario'];
+        return true; //cadastrado com sucesso
+   }
+   else
+   {
+     return false; //não foi possível logar
    }
 
-
-
+   }
 
 }
